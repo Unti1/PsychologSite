429f012f9586891acf072eae7a140fea29c6a42c
Marvelous-A <mogneto.r@mail.ru>
2023-10-09 21:10:00 +0300

    Changes to be committed: 	new file:   card/__pycache__/urls.cpython-311.pyc 	new file:   card/__pycache__/views.cpython-311.pyc 	new file:   card/static/css/card(2).css 	new file:   card/templates/card/base.html 	new file:   card/templates/card/main_list.html 	new file:   card/templates/card/record.html 	new file:   card/urls.py 	modified:   card/views.py 	modified:   settingsSite/__pycache__/urls.cpython-311.pyc 	modified:   settingsSite/urls.py

--- card/views.py
+++ card/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def base(request):
+    return render(request, 'card/main_list.html', {})
+
+def record(request):
+    return render(request, 'card/record.html', {})
