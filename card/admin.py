b0133c44581cb47d1515fd33164e9a352b830aed
Unti1 <kostyuchenko.work@gmail.com>
2023-10-09 22:04:37 +0300

    Changes to be committed: 	modified:   card/__pycache__/__init__.cpython-311.pyc 	modified:   card/__pycache__/admin.cpython-311.pyc 	modified:   card/__pycache__/apps.cpython-311.pyc 	modified:   card/__pycache__/models.cpython-311.pyc 	modified:   card/__pycache__/urls.cpython-311.pyc 	modified:   card/__pycache__/views.cpython-311.pyc 	modified:   card/admin.py 	new file:   card/migrations/0001_initial.py 	new file:   card/migrations/0002_author_age_author_profession_author_stage.py 	new file:   card/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   card/migrations/__pycache__/0002_author_age_author_profession_author_stage.cpython-311.pyc 	modified:   card/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   card/models.py 	renamed:    card/static/css/card(2).css -> card/static/css/card.css 	modified:   card/templates/card/base.html 	modified:   card/urls.py 	modified:   db.sqlite3 	modified:   settingsSite/__pycache__/__init__.cpython-311.pyc 	modified:   settingsSite/__pycache__/settings.cpython-311.pyc 	modified:   settingsSite/__pycache__/urls.cpython-311.pyc 	modified:   settingsSite/__pycache__/wsgi.cpython-311.pyc

--- card/admin.py
+++ card/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from card.models import Author
 
 # Register your models here.
+
+admin.site.register(Author)
--- card/models.py
+++ card/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Author(models.Model):
+    author_title = models.CharField(max_length=256, default='Иван Иванов')
+    phone = models.CharField(max_length=20, default='79999999999')
+    validate_number = models.BooleanField(default=False)
+    description = models.TextField(max_length=5000, null=True)
+    stage = models.IntegerField(null=True)
+    age = models.IntegerField(null=True)
+    profession = models.CharField(max_length=300, null=True)
+
+    def validate_phone_number(self):
+        import phonenumbers
+        try:
+            parsed_number = phonenumbers.parse(self.phone, None)
+            if phonenumbers.is_valid_number(parsed_number):
+                self.validate_number = True
+            else:
+                self.validate_number = False
+        except phonenumbers.phonenumberutil.NumberParseException:
+            self.validate_number = False
+    
+
+    def save(self, *args, **kwargs):
+        self.validate_phone_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return 'Данные на сайте'
+
+class Order(models.Model):
+    ...
